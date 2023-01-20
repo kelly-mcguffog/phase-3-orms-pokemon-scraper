cd1f533b30344fffa8ffba7e090a05c68cdc836e
Kelly McGuffog <kellymcguffog@gmail.com>
2023-01-20 16:22:17 -0500

    commit tree 9e7683e44497b6899b8f07e93d7493301bd93e18

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+    def initialize(id: nil, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?,?);", name, type)
+    end
+
+    def self.find(id, db)
+        match = db.execute("SELECT * FROM pokemon WHERE id=?;", id).flatten
+        Pokemon.new(id: match[0], name: match[1], type: match[2], db:db)
+    end
+
 end
